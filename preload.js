49b265814bcc6522fb49e3455483e09c461c7a93
Troytoilet <160334654+Designmatong@users.noreply.github.com>
2026-06-05 11:20:47 +0800

    Add preload.js

---
+++ preload.js
@@ -0,0 +1,10 @@
+﻿const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  closeWindow: () => ipcRenderer.invoke('window:close'),
+  minimizeWindow: () => ipcRenderer.invoke('window:minimize'),
+  scan: () => ipcRenderer.invoke('scan'),
+  clean: (results) => ipcRenderer.invoke('clean', results),
+  checkAdmin: () => ipcRenderer.invoke('check-admin'),
+  restartAdmin: () => ipcRenderer.invoke('restart-admin'),
+});
